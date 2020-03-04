088ea2347af72716e6f12e2e6d9816659d6bdfef
Rouizi <cinorouizi@hotmail.fr>
2020-03-04 20:09:58 +0100

    debug=true

--- car_rental/settings.py
+++ car_rental/settings.py
@@ -193,7 +193,7 @@ DEFAULT_FILE_STORAGE = 'car_rental.storage_backends.MediaStorage'"""
 # aws settings
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('S3_BUCKET_NAME')
 AWS_DEFAULT_ACL = None
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
