ab51b1c7119d77b7c0953a250bcc43f8dcb90371
Rouizi <cinorouizi@hotmail.fr>
2020-03-04 18:27:58 +0100

    debug=true

--- car_rental/settings.py
+++ car_rental/settings.py
@@ -162,7 +162,7 @@ CELERY_BEAT_SCHEDULE = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
 
 
 INTERNAL_IPS = ['127.0.0.1']
@@ -171,15 +171,23 @@ INTERNAL_IPS = ['127.0.0.1']
 # Amazon Simple Storage Service (S3) to store media file
 # see https://devcenter.heroku.com/articles/s3 for more details
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+#DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID", "") 
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY", "")
 AWS_STORAGE_BUCKET_NAME = os.environ.get("S3_BUCKET_NAME", "")
 AWS_QUERYSTRING_AUTH = False
 AWS_S3_CUSTOM_DOMAIN = os.environ.get("AWS_S3_CUSTOM_DOMAIN", "")
-DEFAULT_FILE_STORAGE = 'car_rental.storage_backends.MediaStorage'
+
 # MEDIA_URL = os.environ.get("MEDIA_URL", "")
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+
+AWS_LOCATION = 'static'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+DEFAULT_FILE_STORAGE = 'car_rental.storage_backends.MediaStorage'
 
 LOGIN_URL = '/users/log_in/'
 
